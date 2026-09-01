b45d35293895afcac5a56b39b318d98a8877ac7b
Bota Andrei Cristian <andreibota188@gmail.com>
2026-09-02 00:48:56 +0300

    Add simple filter

---
+++ mushroom-filter.js
@@ -0,0 +1,46 @@
+const cards = document.querySelectorAll(".mushroom-guide .card");
+const noMatches = document.querySelector(".no-matches");
+const seasonalFilter = document.querySelector("#season");
+const edibleFilter = document.querySelector("#edible");
+
+const currentFilters = {
+  season: "all",
+  edible: "all",
+};
+
+seasonalFilter.addEventListener("change", updateFilter);
+edibleFilter.addEventListener("change", updateFilter);
+
+function updateFilter(e) {
+  const filterType = e.target.name;
+  currentFilters[filterType] = e.target.value;
+  filterCards();
+}
+
+function filterCards() {
+  noMatches.hidden = false;
+  cards.forEach((card) => {
+    const season = card.querySelector("[data-season]").dataset.season;
+    const edible = card.querySelector("[data-edible]").dataset.edible;
+    // console.log(currentFilters.season);
+    // console.log(season);
+    // console.log(season === currentFilters.season);
+    console.log(currentFilters.edible);
+    console.log(edible);
+    card.hidden = true;
+
+    if (season == currentFilters.season || currentFilters.season == "all") {
+      if (edible == currentFilters.edible || currentFilters.edible == "all") {
+        card.hidden = false;
+        noMatches.hidden = true;
+      }
+    }
+  });
+}
+
+function enableFiltering() {
+  seasonalFilter.hidden = false;
+  edibleFilter.hidden = false;
+}
+
+enableFiltering();
