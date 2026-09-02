6d42c6f6a0dc51321180ee89f1039d7394afe410
Bota Andrei Cristian <andreibota188@gmail.com>
2026-09-02 15:18:01 +0300

    Final_Version_Sep_2026

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
   const navOpened = navToggle.getAttribute("aria-expanded");
-  if (navOpened === 'false') {
-    navToggle.setAttribute('aria-expanded', 'true');
+  if (navOpened === "false") {
+    navToggle.setAttribute("aria-expanded", "true");
   } else {
-    navToggle.setAttribute('aria-expanded', 'false');
+    navToggle.setAttribute("aria-expanded", "false");
   }
   console.log(navOpened);
 });
+
+const resizeObserver = new ResizeObserver(() => {
+  document.body.classList("resizing");
+
+  requestAnimationFrame(() => {
+    document.body.classList.remove("resizing");
+  });
+});
+
+resizeObserver.observe(document.body);
